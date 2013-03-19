468b29b63127cc8d5ff4666455118f1b249710fb
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2013-03-19 22:04:51 +0100

    Added ShouldExtensions for single item enumerables.

---
+++ Source/NScripto.Tests/ShouldExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Should;
+
+namespace NScripto.Tests
+{
+    public static class ShouldExtensions
+    {
+        public static T ShouldContainExactlyOne<T>(this IEnumerable<object> objs)
+        {
+            objs.ShouldNotBeNull();
+            objs.Count().ShouldEqual(1);
+            return objs.First().ShouldBeType<T>();
+        }         
+
+        public static T ShouldContainExactlyOne<T>(this IEnumerable<T> objs)
+        {
+            objs.ShouldNotBeNull();
+            objs.Count().ShouldEqual(1);
+            return objs.First();
+        }         
+    }
+}
